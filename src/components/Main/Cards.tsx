12f60bdd6afb50d0cbbde81b373817afb00806d1
utkarsh <55540613+whoisseth@users.noreply.github.com>
2023-05-14 00:58:51 +0530

    fix </div>

--- src/components/Main/Cards.tsx
+++ src/components/Main/Cards.tsx
@@ -11,8 +11,8 @@ const Cards: FC<CardsProps> = ({}) => {
   return (
     <Link href={`/country/1`} className="bg-[#2B3743] rounded overflow-hidden">
       <Image src="/flags/ind.png" width={332} height={208} alt="Flag Image" />
-      <div className="flex flex-col gap-5 px-8 py-7">s
-        <div className="text-xl font-bold">India</div>
+      <div className="flex flex-col gap-5 px-8 py-7">
+        s<div className="text-xl font-bold">India</div>
         <div className="flex flex-col gap-1 text-lg">
           <div>
             <span>Population: </span>
@@ -26,7 +26,7 @@ const Cards: FC<CardsProps> = ({}) => {
             <span>Capital: </span>
             <span>New Delhi</span>
           </div>
-        </div
+        </div>
       </div>
     </Link>
   );
